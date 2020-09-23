82a13901839a892c52b4c05a167c14d38552612e
Crystal T <ctalley.cat@gmail.com>
2020-09-23 17:27:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
-def loop_message_five_times(message) 
-  count = 0 
-  while count < 5
-   puts message = "Hello World."
-  count += 1 
-end
+def loop_message_five_times("Hello World") 
+  
+ 
 end
